a2623f034971abc0de54ad1d5ebb044458fb1f45
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-02-01 09:24:43 +0100

    feat: platform - ios and android

--- App.js
+++ App.js
@@ -1,30 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+import CustomButton from "./components/customButton/CustomButton";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome!</Text>
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Text style={styles.text}>Welcome Here!</Text>
+          {/* use platform file extensions like '.ios' and '.android' for larger parts of the app */}
+          <CustomButton title="PressMe" onPress={() => alert("Pressed")} />
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: "plum",
+  },
   container: {
     flex: 1,
     backgroundColor: "plum",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? 25 : 0, // OS is for smaller parts
   },
   box: {
-    width: 300,
-    height: 300,
-    backgroundColor: "lightblue",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
   },
   text: {
-    fontSize: 24,
+    ...Platform.select({
+      // select is for smaller parts of the app
+      ios: {
+        color: "purple",
+        fontSize: 24,
+        fontStyle: "italic",
+      },
+      android: {
+        color: "blue",
+        fontSize: 30,
+      },
+    }),
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
